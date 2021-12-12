7a8fac61f4ba39b54ea92b65af286ea0d24729b8
thomas messett <thomas.messett@stampapp.me>
2021-12-12 21:22:02 +0000

    updated GQL info

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,9 @@ import { DefaultApolloClient } from '@vue/apollo-composable'
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  // uri: 'https://graph.api.slerp.com/v1/graphql',
-  uri: 'https://graph-qa.api.slerpdemo.com/v1/graphql',
-  // fetchOptions: {
-  // mode: 'no-cors',
-  // },
+  uri: 'https://partner.api.slerp.com/v1/graphql',
   headers: {
-  authorization: 'Bearer ' + 'c0988a900a70346e9c99a3c3ce63647b',
+  authorization: 'Bearer ' + `${process.env.KEY}`,
 
     }
   // credentials: 'include'
